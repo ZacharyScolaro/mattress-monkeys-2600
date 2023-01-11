b9d549c6d00f504e4b197ed3a63302b63123e873
Zack <zachary.scolaro@outlook.com>
2023-01-10 22:38:39 -0500

    Mocking bedposts

--- source/game.c
+++ source/game.c
@@ -8,6 +8,9 @@ static uint8_t colupfBuffer[192];
 __attribute__((section(".noinit")))
 static uint8_t colubkBuffer[192];
 
+#define vcsWrite6(a,d) vcsLda2(d); vcsSta4(a);
+void setPF(int x, int y);
+
 int elf_main(uint32_t* args)
 {
 	// Always reset PC first, cause it's going to be close to the end of the 6507 address space
@@ -42,6 +45,21 @@ int elf_main(uint32_t* args)
 			k++;
 		}
 	}
+	// Draw place holders for wobble
+	for (int i = 0; i < 3; i++)
+	{
+		int right = 36 - (i * 3);
+		int left = right - 32 + (i * 6);
+		for (int j = 0; j < 16; j++)
+		{
+			for (int k = 0; k <= j; k++)
+			{
+				setPF(right-k, j + 48 + (i * 64));
+				setPF(left+k, j + 48 + (i * 64));
+			}
+		}
+	}
+	//Cycle background colors
 	for (int i = 0; i < sizeof(colupfBuffer)/ sizeof(colupfBuffer[0]); i++)
 	{
 		colupfBuffer[i] = i;
@@ -51,24 +69,94 @@ int elf_main(uint32_t* args)
 	// Render loop
 	while (true) {
 		vcsEndOverblank();
-		vcsSta3(WSYNC); vcsSta3(WSYNC); vcsSta3(WSYNC); vcsSta3(WSYNC); vcsSta3(WSYNC); vcsSta3(WSYNC);
-		for (int i = 0; i < 192; i++)
-		{
-			vcsSta3(WSYNC);
-			vcsWrite5(VBLANK, 0);
-			vcsWrite5(COLUPF, colupfBuffer[i]);
-			vcsWrite5(COLUBK, colubkBuffer[i]);
-			vcsWrite5(PF0, ReverseByte[playfieldBuffer[i * 5] >> 4]);
-			vcsWrite5(PF1, (playfieldBuffer[i * 5] << 4) | (playfieldBuffer[i * 5 + 1] >> 4));
-			vcsWrite5(PF2, ReverseByte[(uint8_t)((playfieldBuffer[i * 5 + 1] << 4) | (playfieldBuffer[i * 5 + 2] >> 4))]);
-			vcsWrite5(PF0, ReverseByte[playfieldBuffer[i * 5 + 2]]);
-			vcsJmp3();
-			vcsWrite5(PF1, playfieldBuffer[i * 5 + 3]);
-			vcsJmp3();
-			vcsWrite5(PF2, ReverseByte[playfieldBuffer[i * 5 + 4]]);
-		}
+		vcsSta3(WSYNC); vcsSta3(WSYNC); vcsSta3(WSYNC); vcsSta3(WSYNC); vcsSta3(WSYNC);
+		
 		vcsSta3(WSYNC);
+		vcsNop2n(34);
+		vcsJmp3();
+		vcsWrite5(VBLANK, 0);
+		int line = 0;
+		while(line < 64)
+		{
+			vcsSta3(HMOVE);
+			vcsWrite5(COLUPF, colupfBuffer[line]);
+			vcsWrite5(PF0, ReverseByte[playfieldBuffer[line * 5] >> 4]);
+			vcsWrite5(PF1, (playfieldBuffer[line * 5] << 4) | (playfieldBuffer[line * 5 + 1] >> 4));
+			vcsWrite5(PF2, ReverseByte[(uint8_t)((playfieldBuffer[line * 5 + 1] << 4) | (playfieldBuffer[line * 5 + 2] >> 4))]);
+			vcsWrite5(COLUBK, colubkBuffer[line]);
+			vcsWrite5(PF0, ReverseByte[playfieldBuffer[line * 5 + 2]]);
+			vcsJmp3();
+			vcsWrite5(PF1, playfieldBuffer[line * 5 + 3]);
+			vcsJmp3();
+			vcsWrite5(PF2, ReverseByte[playfieldBuffer[line * 5 + 4]]);
+			vcsJmp3();
+			vcsJmp3();
+			vcsNop2n(6);
+			vcsWrite5(COLUBK, 0xe2);
+			vcsSta3(WSYNC);
+			line++;
+		}
+		while (line < 128)
+		{
+			vcsSta3(HMOVE);
+			vcsWrite5(COLUPF, colupfBuffer[line]);
+			vcsWrite5(PF0, ReverseByte[playfieldBuffer[line * 5] >> 4]);
+			vcsWrite5(PF1, (playfieldBuffer[line * 5] << 4) | (playfieldBuffer[line * 5 + 1] >> 4));
+			vcsJmp3();
+			vcsLdx2(colubkBuffer[line]);
+			vcsWrite5(COLUBK, 0xe2);
+			vcsStx4(COLUBK);
+			vcsWrite5(PF2, ReverseByte[(uint8_t)((playfieldBuffer[line * 5 + 1] << 4) | (playfieldBuffer[line * 5 + 2] >> 4))]);
+			vcsWrite5(PF0, ReverseByte[playfieldBuffer[line * 5 + 2]]);
+			vcsWrite5(PF1, playfieldBuffer[line * 5 + 3]);
+			vcsJmp3();
+			vcsWrite5(PF2, ReverseByte[playfieldBuffer[line * 5 + 4]]);
+			vcsNop2n(3);
+			vcsLdx2(0x5c);
+			vcsWrite5(COLUBK, 0xe2);
+			vcsStx4(COLUBK);
+			vcsSta3(WSYNC);
+			line++;
+		}
+		while (line < 192)
+		{
+			vcsSta3(HMOVE);
+			vcsWrite5(COLUPF, colupfBuffer[line]);
+			vcsWrite5(PF0, ReverseByte[playfieldBuffer[line * 5] >> 4]);
+			vcsWrite6(PF1, (playfieldBuffer[line * 5] << 4) | (playfieldBuffer[line * 5 + 1] >> 4));
+			vcsWrite6(PF2, ReverseByte[(uint8_t)((playfieldBuffer[line * 5 + 1] << 4) | (playfieldBuffer[line * 5 + 2] >> 4))]);
+			vcsLdx2(colubkBuffer[line]);
+			vcsWrite5(COLUBK, 0xe2);
+			vcsStx4(COLUBK);
+			vcsWrite5(PF0, ReverseByte[playfieldBuffer[line * 5 + 2]]);
+			vcsWrite5(PF1, playfieldBuffer[line * 5 + 3]);
+			vcsJmp3();
+			vcsWrite5(PF2, ReverseByte[playfieldBuffer[line * 5 + 4]]);
+			vcsJmp3();
+			vcsLdx2(0x5c);
+			vcsWrite5(COLUBK, 0xe2);
+			vcsStx4(COLUBK);
+			vcsSta3(WSYNC);
+			line++;
+		}
 		vcsWrite5(VBLANK, 2);
 		vcsStartOverblank();
 	}
 }
+
+void setPF(int x, int y)
+{
+	int offset = x / 8;
+	uint8_t mask = 0x80 >> (x - (offset * 8));
+	playfieldBuffer[y * 5 + offset] |= mask;
+}
+
+/*
+
+_XXX___XXX___XXX___XXX___XXX___XXX___XXX___XXX
+__HH                                       HHH
+__  HHH                                 HHH   
+__     HHH                           HHH      
+
+
+*/
